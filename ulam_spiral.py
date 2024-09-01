fd3bee2e5710773438c2122df83f94dd88048f9d
Daniel P. <147325072+dani238qr@users.noreply.github.com>
2024-09-01 19:35:29 +0300

    Update ulam_spiral.py

--- ulam_spiral.py
+++ ulam_spiral.py
@@ -2,8 +2,13 @@ import turtle
 import time
 import math
 
-max_n=int(input("Size of spiral: "))
-font_size=int(input("Font size(3 or smaller for spiral > 2000: "))
+
+try:
+    max_n=int(input("Size of spiral: "))
+    font_size= int(input("Font size(3 or smaller for spiral > 2000): "))
+except ValueError:
+    print("Please enter valid integers for size and font size.")
+    return
 
 def is_prime(n):
     if n<=1:
